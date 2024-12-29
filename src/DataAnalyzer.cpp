c8e836e1cfe9f72fcf1dfed4d92796431363d775
Przemyslaw Majerczak <przmajerczak@gmail.com>
2024-12-29 19:15:40 +0100

    Add centering display on trigger

--- src/DataAnalyzer.cpp
+++ src/DataAnalyzer.cpp
@@ -6,10 +6,35 @@
 
 void DataAnalyzer::handleData(const RawValuesContainer &current_values)
 {
-    [[maybe_unused]] const auto triggersIndexes{detectTriggers(current_values)};
+    RawValuesContainer valuesToDisplay{centerValuesOnTrigger(current_values)};
 
     SettingsWindow::notifyAboutThresholdTrigger();
-    DisplayHelper::triggerDisplay(current_values);
+    DisplayHelper::triggerDisplay(valuesToDisplay);
+}
+
+RawValuesContainer DataAnalyzer::centerValuesOnTrigger(const RawValuesContainer &current_values)
+{
+    const auto triggersIndexes{detectTriggers(current_values)};
+    const std::size_t selectedTrigger{triggersIndexes.at(triggersIndexes.size() / 2)};
+    const int shiftCountForTriggerCenter{static_cast<int>(selectedTrigger - (X_SIZE / 2))};
+
+    RawValuesContainer valuesToDisplay;
+    valuesToDisplay.resize(current_values.size());
+
+    for (std::size_t idx = 0; idx < valuesToDisplay.size(); ++idx)
+    {
+        const std::size_t current_values_idx{idx + shiftCountForTriggerCenter + 2};
+        if (current_values_idx >= 0 and current_values_idx < current_values.size())
+        {
+            valuesToDisplay.at(idx) = current_values.at(current_values_idx);
+        }
+        else
+        {
+            valuesToDisplay.at(idx) = 0xffff;
+        }
+    }
+
+    return valuesToDisplay;
 }
 
 std::vector<std::size_t> DataAnalyzer::detectTriggers(const RawValuesContainer &current_values)
--- src/DataAnalyzer.hpp
+++ src/DataAnalyzer.hpp
@@ -12,6 +12,7 @@ public:
     void handleData(const RawValuesContainer &current_values);
 
 private:
+    RawValuesContainer centerValuesOnTrigger(const RawValuesContainer &current_values);
     std::vector<std::size_t> detectTriggers(const RawValuesContainer &current_values);
     bool isTrigger(const uint16_t leftValue, const uint16_t rightValue);
 };
