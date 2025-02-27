a5d20d5004f1801fe5694bf6ddaeaf0588516766
Przemyslaw Majerczak <przmajerczak@gmail.com>
2025-02-27 19:49:45 +0100

    Vertical controls refactor
    Make vertical controls in fact vertical

--- src/settingsWindow/controls/VerticalBoundControls.cpp
+++ src/settingsWindow/controls/VerticalBoundControls.cpp
@@ -34,16 +34,20 @@ void verticalUpperBoundSliderOnChangeAction(GtkRange *range,
 void VerticalBoundControls::prepare(DynamicData &dynamicData)
 {
     vertical_lower_bound_slider = gtk_scale_new_with_range(
-        GTK_ORIENTATION_HORIZONTAL, INPUT_SIGNAL_MIN, INPUT_SIGNAL_MAX, 1);
+        GTK_ORIENTATION_VERTICAL, INPUT_SIGNAL_MIN, INPUT_SIGNAL_MAX, 1);
     gtk_widget_set_hexpand(vertical_lower_bound_slider, TRUE);
     gtk_scale_set_draw_value(GTK_SCALE(vertical_lower_bound_slider), TRUE);
     gtk_range_set_value(GTK_RANGE(vertical_lower_bound_slider), INPUT_SIGNAL_MIN);
+    gtk_range_set_inverted(GTK_RANGE(vertical_lower_bound_slider), TRUE);
+    gtk_widget_set_size_request(vertical_lower_bound_slider, 0, 300);
 
     vertical_upper_bound_slider = gtk_scale_new_with_range(
-        GTK_ORIENTATION_HORIZONTAL, INPUT_SIGNAL_MIN, INPUT_SIGNAL_MAX, 1);
+        GTK_ORIENTATION_VERTICAL, INPUT_SIGNAL_MIN, INPUT_SIGNAL_MAX, 1);
     gtk_widget_set_hexpand(vertical_upper_bound_slider, TRUE);
     gtk_scale_set_draw_value(GTK_SCALE(vertical_upper_bound_slider), TRUE);
     gtk_range_set_value(GTK_RANGE(vertical_upper_bound_slider), INPUT_SIGNAL_MAX);
+    gtk_range_set_inverted(GTK_RANGE(vertical_upper_bound_slider), TRUE);
+    gtk_widget_set_size_request(vertical_upper_bound_slider, 0, 300);
 
     callbackDataForLowerBoundSlider.data = &dynamicData;
     callbackDataForLowerBoundSlider.widget = vertical_upper_bound_slider;
@@ -62,15 +66,21 @@ void VerticalBoundControls::prepare(DynamicData &dynamicData)
 
 GtkWidget *VerticalBoundControls::getVerticalBoundControlsContainer()
 {
-    GtkWidget *verticalBoundControlsExpander = gtk_expander_new("Vertical bound controls");
+    GtkWidget *verticalBoundControlsExpander =
+        gtk_expander_new("Vertical bound controls");
     gtk_expander_set_expanded(GTK_EXPANDER(verticalBoundControlsExpander), TRUE);
 
-    GtkWidget *verticalBoundControlsGrid = gtk_grid_new();
-    gtk_grid_attach(GTK_GRID(verticalBoundControlsGrid),
-                    vertical_lower_bound_slider, 0, 0, 1, 1);
-    gtk_grid_attach(GTK_GRID(verticalBoundControlsGrid),
-                    vertical_upper_bound_slider, 0, 1, 1, 1);
+    constexpr int spacing{0};
+    GtkWidget *verticalBoundControlsHorizontalBox =
+        gtk_box_new(GTK_ORIENTATION_HORIZONTAL, spacing);
 
-    gtk_container_add(GTK_CONTAINER(verticalBoundControlsExpander), verticalBoundControlsGrid);
+    constexpr int padding{0};
+    gtk_box_pack_start(GTK_BOX(verticalBoundControlsHorizontalBox),
+                       vertical_lower_bound_slider, FALSE, TRUE, padding);
+    gtk_box_pack_start(GTK_BOX(verticalBoundControlsHorizontalBox),
+                       vertical_upper_bound_slider, FALSE, TRUE, padding);
+
+    gtk_container_add(GTK_CONTAINER(verticalBoundControlsExpander),
+                      verticalBoundControlsHorizontalBox);
     return verticalBoundControlsExpander;
 }
