6aae684602a125b086cc292dd29f83a71d32d35c
Przemyslaw Majerczak <przmajerczak@gmail.com>
2025-03-02 01:47:29 +0100

    Timemarkers introduction
    Extracting code to StatsForTimemarker.cpp

---
+++ src/debug/StatsForTimemarker.cpp
@@ -0,0 +1,22 @@
+#include "StatsForTimemarker.hpp"
+
+void StatsForTimemarker::logDuration_us(const uint16_t duration_us)
+{
+    durations_us.push_back(duration_us);
+
+    while (durations_us.size() > number_of_measurements)
+    {
+        durations_us.pop_front();
+    }
+}
+
+uint32_t StatsForTimemarker::getAverageDuration_us() const
+{
+    if (durations_us.empty())
+    {
+        return 0;
+    }
+
+    return std::accumulate(durations_us.begin(), durations_us.end(), 0) /
+           durations_us.size();
+}
--- src/debug/StatsForTimemarker.hpp
+++ src/debug/StatsForTimemarker.hpp
@@ -8,27 +8,8 @@
 class StatsForTimemarker
 {
 public:
-    void logDuration_us(const uint16_t duration_us)
-    {
-        durations_us.push_back(duration_us);
-
-        while (durations_us.size() > number_of_measurements)
-        {
-            durations_us.pop_front();
-        }
-    }
-
-    uint32_t getAverageDuration_us() const
-    {
-
-        if (durations_us.empty())
-        {
-            return std::numeric_limits<double>::infinity();
-        }
-
-        return std::accumulate(durations_us.begin(), durations_us.end(), 0) /
-               durations_us.size();
-    }
+    void logDuration_us(const uint16_t duration_us);
+    uint32_t getAverageDuration_us() const;
 
 private:
     std::list<uint16_t> durations_us;
