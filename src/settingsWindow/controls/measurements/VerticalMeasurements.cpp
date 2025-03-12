edc200f6ad9cfc6e11228c5ca0f6e0700e5b3a6e
Przemyslaw Majerczak <przmajerczak@gmail.com>
2025-03-12 22:21:51 +0100

    VerticalMeasurements adjustment

--- src/settingsWindow/controls/measurements/VerticalMeasurements.cpp
+++ src/settingsWindow/controls/measurements/VerticalMeasurements.cpp
@@ -7,35 +7,37 @@ void measuredVoltageSliderOnChangeAction(GtkRange *range, gpointer data) {}
 
 void VerticalMeasurements::prepare(DynamicData &dynamicData)
 {
-    GtkAdjustment *lower_bound_slider_adjustment = gtk_adjustment_new(
-        INPUT_SIGNAL_MIN, INPUT_SIGNAL_MIN, INPUT_SIGNAL_MAX, 1, 0.0, 0.0);
-    baseline_voltage_slider =
-        gtk_scale_new(GTK_ORIENTATION_VERTICAL, lower_bound_slider_adjustment);
+    GtkAdjustment *baseline_voltage_slider_adjustment = gtk_adjustment_new(
+        Y_DISPLAY_RESOLUTION * 0.3, 0, Y_DISPLAY_RESOLUTION, 1, 0.0, 0.0);
+    baseline_voltage_slider = gtk_scale_new(GTK_ORIENTATION_VERTICAL,
+                                            baseline_voltage_slider_adjustment);
     gtk_widget_set_hexpand(baseline_voltage_slider, TRUE);
     gtk_scale_set_draw_value(GTK_SCALE(baseline_voltage_slider), FALSE);
     gtk_range_set_inverted(GTK_RANGE(baseline_voltage_slider), TRUE);
-    gtk_widget_set_size_request(baseline_voltage_slider, 0, DEFAULT_SLIDER_HEIGHT);
+    gtk_widget_set_size_request(baseline_voltage_slider, 0,
+                                DEFAULT_SLIDER_HEIGHT);
 
-    GtkAdjustment *upper_bound_slider_adjustment = gtk_adjustment_new(
-        INPUT_SIGNAL_MAX, INPUT_SIGNAL_MIN, INPUT_SIGNAL_MAX, 1, 0.0, 0.0);
-    measured_voltage_slider =
-        gtk_scale_new(GTK_ORIENTATION_VERTICAL, upper_bound_slider_adjustment);
+    GtkAdjustment *measurement_voltage_slider_adjustment = gtk_adjustment_new(
+        Y_DISPLAY_RESOLUTION * 0.6, 0, Y_DISPLAY_RESOLUTION, 1, 0.0, 0.0);
+    measured_voltage_slider = gtk_scale_new(
+        GTK_ORIENTATION_VERTICAL, measurement_voltage_slider_adjustment);
     gtk_widget_set_hexpand(measured_voltage_slider, TRUE);
     gtk_scale_set_draw_value(GTK_SCALE(measured_voltage_slider), FALSE);
     gtk_range_set_inverted(GTK_RANGE(measured_voltage_slider), TRUE);
-    gtk_widget_set_size_request(measured_voltage_slider, 0, DEFAULT_SLIDER_HEIGHT);
+    gtk_widget_set_size_request(measured_voltage_slider, 0,
+                                DEFAULT_SLIDER_HEIGHT);
 
     g_signal_connect(baseline_voltage_slider, "value-changed",
                      G_CALLBACK(baselineVoltageSliderOnChangeAction), nullptr);
 
     baseline_voltage_spin_button =
-        gtk_spin_button_new(lower_bound_slider_adjustment, 1.0, 0);
+        gtk_spin_button_new(baseline_voltage_slider_adjustment, 1.0, 0);
 
     g_signal_connect(measured_voltage_slider, "value-changed",
                      G_CALLBACK(measuredVoltageSliderOnChangeAction), nullptr);
 
     measured_voltage_spin_button =
-        gtk_spin_button_new(upper_bound_slider_adjustment, 1.0, 0);
+        gtk_spin_button_new(measurement_voltage_slider_adjustment, 1.0, 0);
 }
 
 GtkWidget *VerticalMeasurements::getVerticalMeasurementsContainer()
