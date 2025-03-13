a294950b597898fcb1c0e3b378df3fa2002e5ab2
Przemyslaw Majerczak <przmajerczak@gmail.com>
2025-03-13 22:04:23 +0100

    VerticalMeasurementsData expansion

--- src/sharedData/VerticalMeasurementsData.cpp
+++ src/sharedData/VerticalMeasurementsData.cpp
@@ -2,10 +2,12 @@
 
 void VerticalMeasurementsData::notifyAboutBaselineChange(const float new_baseline)
 {
-    notifyAboutAChange(new_baseline);
+    baseline = new_baseline;
+    baseline_mV = scaleYToVoltage_mV(new_baseline);
 }
 
 void VerticalMeasurementsData::notifyAboutMeasurementChange(const float new_measurement)
 {
-    notifyAboutBChange(new_measurement);
+    measurement = new_measurement;
+    measurement_mV = scaleYToVoltage_mV(new_measurement);
 }
--- src/sharedData/VerticalMeasurementsData.hpp
+++ src/sharedData/VerticalMeasurementsData.hpp
@@ -1,22 +1,29 @@
 #pragma once
 
 #include "VerticalDataBase.hpp"
+#include "constants.hpp"
 
-class VerticalMeasurementsData : public VerticalDataBase
+class VerticalMeasurementsData
 {
 public:
     bool active{false};
 
-    float &baselineIndicator() { return a; }
-    float &measurementIndicator() { return b; }
-    float &baselineIndicator_mV() { return a_mV; }
-    float &measurementIndicator_mV() { return b_mV; }
+    float &baselineIndicator() { return baseline; }
+    float &measurementIndicator() { return measurement; }
+    float &baselineIndicator_mV() { return baseline_mV; }
+    float &measurementIndicator_mV() { return measurement_mV; }
 
-    const float baselineIndicator() const { return a; }
-    const float measurementIndicator() const { return b; }
-    const float baselineIndicator_mV() const { return a_mV; }
-    const float measurementIndicator_mV() const { return b_mV; }
+    const float baselineIndicator() const { return baseline; }
+    const float measurementIndicator() const { return measurement; }
+    const float baselineIndicator_mV() const { return baseline_mV; }
+    const float measurementIndicator_mV() const { return measurement_mV; }
 
     void notifyAboutBaselineChange(const float new_baseline);
     void notifyAboutMeasurementChange(const float new_measurement);
+
+private:
+    float baseline{Y_DISPLAY_RESOLUTION * 0.3};
+    float measurement{Y_DISPLAY_RESOLUTION * 0.6};
+    float baseline_mV{MIN_VOLTAGE_mV};
+    float measurement_mV{MAX_VOLTAGE_mV};
 };
